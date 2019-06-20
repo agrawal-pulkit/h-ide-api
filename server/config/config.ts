cc530b84fe57322cb25c6fb5ad562c963d21cfda
Pulkit Agrawal <pulkit@rapido.bike>
2019-06-21 01:49:56 +0530

    updates to deploy to heroku

--- server/config/config.ts
+++ server/config/config.ts
@@ -8,7 +8,7 @@ class EnvConfig {
     }
 
     private assignConfig() {
-        if (this.env === 'dev' || this.env === 'test') {
+        if (this.env === 'dev' || this.env === 'test' || this.env === 'production') {
             const config = require('./config.json');
             const envConfig = config[this.env];
             Object.keys(envConfig).forEach((key) => {
